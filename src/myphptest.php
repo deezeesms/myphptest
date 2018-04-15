aa9c227daff9c55afaddb4ac1d658ac69d01fbae
deezeesms <deezeesms@gmail.com>
2018-04-15 08:49:38 -0400

    the the content of php

--- src/myphptest.php
+++ src/myphptest.php
@@ -2,17 +2,10 @@
 <html>
 <body>
 
+<h1>My PHP page</h1>
+
 <?php
-// This is a single-line comment
-# This is also a single-line comment
-/*
-This is a multiple-lines comment block
-that spans over multiple
-lines
-*/
-// You can also use comments to leave out parts of a code line
-$x = 5 /* + 15 */ + 5;
-echo $x;
+echo "Hello World!";
 ?>
 
 </body>
